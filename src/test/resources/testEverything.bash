eef057f15c52d44418397e7eda2160b533bc0889
Koldar <mbono@unibs.it>
2017-09-30 15:32:32 +0200

    ADded automated script
    - this script will retest every test inside test.c file;
    - you need to execute it inside crashc main directory

---
+++ src/test/resources/testEverything.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+#we need to go to src/test/c, aka the place where all the tests are present
+#we fetch all the issues id. There is contract:
+# 1) every file is in the form test_issue%04d.c where "%04d" is the id of the issue.
+# 2) the issue source code is always included in #ifdef, actitable by a define macro called TEST_%04d where "%04d" is the id of the issue
+#
+# so, we fetch all the issues in side src/test/c; then, for each of them, we compile
+
+cd .
+if test `basename $(dirname $(realpath .))` != "crashc"
+then
+	echo "You are not in 'crashc' folder! I won't execute this script because I'm not sure this will work!"
+	exit 2
+fi
+CRASHC_MAINFOLDER=`pwd`
+
+mkdir -pv build/Debug
+
+cd src/test/c
+if test $? -ne 0
+then
+	echo "going into src/test/c has failed. Exiting!"
+	exit 2
+fi
+CRASHC_TEST_C=`pwd`
+cd "${CRASHC_MAINFOLDER}/build/Debug"
+if test $? -eq 0
+then
+	echo "going into build/Debug has failed. Exiting!"
+	exit 2
+fi
+CRASHC_BUILD_DEBUG=`pwd`
+
+
+
+OLDPWD=`pwd`
+
+cd ${CRASHC_BUILD_DEBUG}
+	for issueID in `ls -l ${CRASHC_TEST_C}| grep -E "issue[0-9]+\." | sed 's/  */ /g' | cut -d" " -f9 | sed 's/test_issue\([0-9][0-9]*\)\.c/\1/g'`
+	do
+		#alter CMakeFiles.txt in the main directory
+		sed -i "s/add_definitions(-DTEST_[0-9][0-9]*)/add_definitions(-DTEST_0020)/" "${CRASHC_MAINFOLDER}/CMakeLists.txt"
+		#now we compile everything
+	
+		cmake ../.. && make && clear && ./Test > output.${issueId}.txt
+		#we ensure the just created output has no KO
+		if test `cat output.txt | grep "KO" | wc -l` -gt 0
+		then
+			echo "Error while executing test ${issueID}!"
+			cd ${CRASHC_MAIN_FOLDER}
+			exit 1
+		fi
+	done
+
+cd $OLDPWD
+echo "EVERYTHING HAS BEEN DONE!"
