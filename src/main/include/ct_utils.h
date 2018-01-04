5d1b643d2dee57806b4020a8f2ed372311831540
Koldar <mbono@unibs.it>
2018-01-04 23:32:25 +0100

    fully documented ct_utils.h

--- src/main/include/ct_utils.h
+++ src/main/include/ct_utils.h
@@ -1,8 +1,10 @@
-/*
- * ct_utils.h
+/**
+ * @file
  *
- *  Created on: Oct 31, 2017
- *      Author: noodles
+ * Contains generic utilities used throughout all the project
+ *
+ * @author noodles
+ * @date Oct 31, 2017
  */
 
 #ifndef CT_UTILS_H_
@@ -11,10 +13,18 @@
 //TODO: Maybe improve this function
 /**
  * Returns the basename of a file, given its absolute path.
- * Note that this function is written specifically to fit CrashC needs: we get the file name
- * from the __FILE__ macro, which always expand to an absolute filename. Due to the fact that
+ *
+ * \note
+ * This function is written specifically to fit @crashc needs: we get the file name
+ * from the \c __FILE__ macro, which always expand to an absolute filename. Due to the fact that
  * the filename is included compile-time in the binary we also return a pointer to a specific
  * offset in the binary in order not to need any dynamic allocation
+ *
+ * TODO is this example correct?
+ * For example, if the input is <tt>/home/koldar/crashc/mytest.c</tt> the return value will be <tt>mytest.c</tt>
+ *
+ * @param[in] filename the absolute filename returned by \c __FILE__
+ * @return the basename of the absolute file
  */
 char * ct_file_basename(const char * filename);
 
