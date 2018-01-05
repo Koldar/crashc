a15cbfe6fc09450303c11c2cd6ac218aff9b568a
Koldar <mbono@unibs.it>
2018-01-05 19:44:49 +0100

    typedefs.h fully documented

--- src/main/include/typedefs.h
+++ src/main/include/typedefs.h
@@ -1,10 +1,10 @@
-/*
- * \file typedefs.h
+/**
+ * @file
  *
- * Contains all the general purpose typedefs used in the project
+ * Contains all the general purpose typedefs used in @crashc
  *
- *  Created on: Oct 11, 2017
- *      Author: koldar
+ * @author koldar
+ * @date Oct 11, 2017
  */
 
 #ifndef TYPEDEFS_H_
@@ -34,45 +34,69 @@ typedef int SectionLevelId;
 /**
  * Represents the signature of a function which release a structure from the memory
  *
- * \li The first parameter is the structure to release from the memory
+ * @param[inout] obj the structure to release from the memory
  */
-typedef void (*ct_destructor_t)(void*);
+typedef void (*ct_destructor_t)(void* obj);
 
 /**
- * Function pointer used to point at the functions which contain the testsuites.
+ * Function signature used to point at the functions which represent @testsuite
  */
 typedef void (*test_pointer)(void);
 
 /**
- * This type defines the function pointer to the function used to  produce the report of a single test.
- * This function usually calls a ct_snapshot_tree_reporter.
+ * @addtogroup reportFunctionType Report Function Types
+ * @brief function types used in ::ct_report_producer_t in order to build a test report human readable
+ * @{
  */
-typedef void (*ct_test_reporter_t)(ct_model_t *, ct_test_report_t *);
+
+/**
+ * This type defines the function pointer to the function used to produce the report of a single test.
+ *
+ * This function usually calls a ct_snapshot_tree_reporter.
+ *
+ * @param[inout] model the model under analysis
+ * @param[in] test_report the test report we need to create a human readable representation of
+ */
+typedef void (*ct_test_reporter_t)(ct_model_t * model, ct_test_report_t * test_report);
 
 /**
  * This type defines the function pointer to the function used to produce the report of a snapshot tree
+ *
+ * @param[inout] model the model under analysis
+ * @param[in] snapshot the ::SectionSnapshot we need to create a human readable representation of
+ * @param[in] level the depth (in the snapshot tree) of the \c snapshot
  */
-typedef void (*ct_snapshot_tree_reporter_t)(ct_model_t *, SectionSnapshot *, int);
+typedef void (*ct_snapshot_tree_reporter_t)(ct_model_t * model, SectionSnapshot * snapshot, int level);
 
 /**
  * This type defines the function pointer to the function used to produce the tests summary.
+ *
  * The statistics of the tests, if used by the model, can be reached through it, so they are not passed
  * as an argument explicitly
+ *
+ * @param[inout] model the model to use to create the human readable report of the statistics
  */
-typedef void (*ct_summary_reporter_t)(ct_model_t *);
+typedef void (*ct_summary_reporter_t)(ct_model_t * model);
 
 /**
- * This type defines the function pointer to the function used to produce the reports for the assertions
- * executed during a specific snapshot. The SectionSnapshot argument is used to get the list of ct_assert_report_t*.
- * The int parameter is used to specify the indentation level to use.
+ * This type defines the function pointer to the function used to produce the reports for the assertions executed during a specific snapshot.
+ *
+ * @param[inout] model the model under analysis
+ * @param[in] snapshot the ::SectionSnapshot containing the relevant asseritons. This argument **can be used** to get the list of ct_assert_report_t.
+ * @param[in] level the depth (in the snapshot tree) of the \c snapshot
  */
-typedef void (*ct_assert_reporter_t)(ct_model_t*, SectionSnapshot*, int);
+typedef void (*ct_assert_reporter_t)(ct_model_t* model, SectionSnapshot* snapshot, int level);
 
 /**
- * This type defines the function pointer to the function used to create the whole report by calling
- * the above functions. The list of test reports is passed through the model. In the same way it is possible to reach
+ * function pointer type used to create the whole report by calling the other \ref reportFunctionType.
+ *
+ * The list of test reports is passed through the model. In the same way it is possible to reach
  * the report_producer implementation.
+ *
+ * @param[inout] model the model to consider
  */
-typedef void (*ct_reporter_t)(ct_model_t *);
+typedef void (*ct_reporter_t)(ct_model_t * model);
+
+///@}
 
 #endif /* TYPEDEFS_H_ */
