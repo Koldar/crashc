0709401b867611b498690f16e144d069182c3fa5
Lorenzo Nodari <lorenzo.nodari.it@ieee.org>
2018-01-23 00:17:46 +0100

    Fixed test for issue#71

--- src/test/c/test_issue0071.c
+++ src/test/c/test_issue0071.c
@@ -15,9 +15,14 @@ REG_SUITE(0);
 TESTS_END
 
 TESTSUITE(0) {
-	int sum = 0;
-	sum += 1;
-	ASSERT(sum >= 0 && sum <= 10);
+	//This testcase is here only to prevent the test to fail, as the issue is marked as wontfix
+	// due to it being related to an unsupported feature of CrashC in the version in which the issue
+	// was created.
+	TESTCASE("testcase", "") {
+		int sum = 0;
+		sum += 1;
+		ASSERT(sum >= 0 && sum <= 10);
+	}
 }
 
 #endif
